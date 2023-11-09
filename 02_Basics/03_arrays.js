5fd20c81d3c1daa01d351ba25dc04d01273d507f
Prasadkare77 <prasadkare77@gmail.com>
2023-11-09 21:26:25 +0530

    array in javascript | PACKED vs HOLEY

--- 02_Basics/03_arrays.js
+++ 02_Basics/03_arrays.js
@@ -24,3 +24,33 @@ arrTwo[10] = 11
 console.log(arrTwo)
 console.log(arrTwo.length)
 console.log(arrTwo[9])
+
+// bound check
+// hasOwnProperty(arrtTwo, 9)
+// hasOwnProperty(arrtTwo.prototype, 10)
+// hasOwnProperty(Object.prototype, 10)
+
+// Holes Are very expensive in Javascript
+
+
+const arrThree = [1, 2, 3, 4, 5]
+console.log(arrThree[2])
+
+// SMI >> Double >> Packed
+// H_SMI > H_Double > H_Packed
+
+const arrFour = new Array(3)
+// Just Three Holes. HOLEY_SMI_ELEMENTS
+arrFour[0] = '1' // HOLEY_ELEMENTS
+arrFour[1] = '2' // HOLEY_ELEMENTS
+arrFour[2] = '3' // HOLEY_ELEMENTS
+
+const arrFive = []
+arrFive.push('1') // PACKED
+arrFive.push('2') // PACKED
+arrFive.push('3') // PACKED
+
+const arrSix = [1, 2, 3, 4, 5]
+arrSix.push(Infinity) //PACKED_DOUBLE_ELEMENTS
+
+// for, for-of, forEach
